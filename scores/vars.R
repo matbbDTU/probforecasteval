d0c2434dcbfc4f0e5d2546b56feb12c5c8c13916
matbbDTU <graskian@gmail.com>
2019-02-28 11:37:51 +0100

    Update vars.R

--- scores/vars.R
+++ scores/vars.R
@@ -1,19 +1,20 @@
-vars <- function(f,y,p){
-  
-  n <- dim(f)[1] # Size of ensemble
-  d <- dim(f)[2] # Dimension
+vars <- function(x,y,p=0.5){
+
+  m <- dim(x)[1] # Size of ensemble
+  k <- dim(x)[2] # Maximal forecast horizon
   
   # Iterate through all pairs
   score <- 0
-  for(i in 1:(d-1)){
-    for(j in (i+1):d){
+  for(i in 1:(k-1)){
+    for(j in (i+1):k){
       
-      Ediff <- 1/n*sum(abs(f[,i]-f[,j])^p)
+      Ediff <- 1/m*sum(abs(x[,i]-x[,j])^p)
       score <- score + (abs(y[i]-y[j])^p - Ediff)^2
       
     }
   }
   
-  # Result
+  # Variogram score
   return(score)
+  
 }

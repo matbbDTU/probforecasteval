d1fa8b5eae002e49ad2b0dc83d2b7fdb9cd685ad
matbbDTU <graskian@gmail.com>
2023-07-11 03:27:27 +0200

    Update packages_install.R

--- packages_install.R
+++ packages_install.R
@@ -1,3 +1,5 @@
 install.packages('ks')
-install.packages('Emcdf')
-install.packages('R2Cuba')
+install.packages('cubature')
+
+# The Emcdf package is no longer available from CRAN, but can be download here: 
+# https://cran.r-project.org/src/contrib/Archive/Emcdf/

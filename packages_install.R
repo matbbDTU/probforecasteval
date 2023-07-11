c11988f4a2383298854d09db33731b90afdae4b8
matbbDTU <graskian@gmail.com>
2023-07-11 03:29:58 +0200

    Update packages_install.R
    R2Cuba and Emcdf are no longer available at CRAN. Solution:
    
    Replaced R2Cuba with cubature.
    Emcdf is installed by following a link with instructions.

--- packages_install.R
+++ packages_install.R
@@ -3,3 +3,4 @@ install.packages('cubature')
 
 # The Emcdf package is no longer available from CRAN, but can be download here: 
 # https://cran.r-project.org/src/contrib/Archive/Emcdf/
+# Install from Rstudio by selecting Tools -> Install Packages... and select the appropriate tar.gz file.
